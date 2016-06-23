480621c93de28130afa349c64efd96c24adfed46
Iguchi Tomokatsu <iguchi.tomokatsu@designone.jp>
2016-06-23 20:04:59 +0900

    1.21

---
+++ 1.21.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(define (square n)
+  (* n n))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((devides? test-divisor n) test-divisor)
+        (else
+         (find-divisor n (+ test-divisor 1)))))
+
+(define (devides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+;; root n
+
+;; log n フェルマーの小定理 確実ではない
+(define (fe
