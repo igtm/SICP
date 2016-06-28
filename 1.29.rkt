1a26f4ded7af34e2e6ba410e948b2c64b3ce8d0b
Iguchi Tomokatsu <iguchi.tomokatsu@designone.jp>
2016-06-28 19:57:02 +0900

    1.29

--- 1.29.rkt
+++ 1.29.rkt
@@ -3,23 +3,25 @@
   (define (h)
     (/ (- b a) n))
   (define (y k)
-    (+ a (* k (h))))
+    (f (+ a (* k (h)))))
   (define (coefficient k)
     (cond
       ((= k 0) 1)
       ((= k n) 1)
       ((even? k) 4)
-      (else 4)))
-  (define (f k)
+      (else 2)))
+  (define (proc k)
     (* (coefficient k) (y k)))
-  (* (/ h 3) (sum (f 0) 0 inc n)))
+  (* (/ (h) 3) (sum proc 0 inc n)))
 
 
 (define (inc a)
   (+ a 1))
 
 (define (sum term a next b)
-      (> a b)
+      (if (> a b)
        0
        (+ (term a)
-          (sum term (next a) next b)))
+          (sum term (next a) next b))))
+(define (cube a)
+  (* a a a))
